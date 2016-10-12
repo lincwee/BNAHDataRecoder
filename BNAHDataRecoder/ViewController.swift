b7f40cbfe311b04321c48ff3fb941b1a6cd23b1a
linwei.hao <linwei.hao@qunar.com>
2016-10-12 15:07:15 +0800

    Initial Commit

---
+++ BNAHDataRecoder/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BNAHDataRecoder
+//
+//  Created by 郝 林巍 on 2016/10/12.
+//  Copyright © 2016年 lincwee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
